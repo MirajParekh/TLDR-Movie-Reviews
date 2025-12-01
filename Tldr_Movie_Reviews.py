8267cbf77fe4e785d95bac3875c1d41ba37a4f34
Miraj Parekh <mirajpar@Mirajs-MacBook-Pro.local>
2025-12-02 00:18:31 +0100

    Found data to eventually provide nn; loaded csv data with pandas; changed sentiment data column from strings to integers to be easily interpreted by model

--- Tldr_Movie_Reviews.py
+++ Tldr_Movie_Reviews.py
@@ -1,10 +1,32 @@
+"""
+Utilizing sentiment analysis to determine whether
+an inputted movie review is positive or negative
+
+Purpose: to work with important libraries and gain a grasp of NLP
+"""
+
+__author__ = "Miraj Parekh"
+
+import torch
+import pandas as pd
+from torch import nn
 from transformers import AutoTokenizer
 
-# 1. Load the tokenizer from the cloud (downloads the dictionary once)
+# 'tokenizer' is an instance of tokenizer class - provides ability to use methods 
 tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-uncased")
 
-text = "this movie is lowkey the best movie i think i've ever seen, I cant lie"
+# text = "this movie is lowkey the best movie i think i've ever seen, I cant lie"
+# tokens = tokenizer(text)
 
-tokens = tokenizer(text)
+# take data csv file and give raw data 
+df = pd.read_csv("dataset.csv.zip")
+print(df.head())
 
-print(tokens)
+# convert sentiment column from strings to ints ('positive' - 1; 'negative' - 0)
+def sentiment_conversion(word):
+    if word == "positive":
+        return 1
+    return 0
+
+df["sentiment"] = df["sentiment"].apply(sentiment_conversion) # <- letting pandas take care of it, thats why we exclude the parameter here bcus it wont work
+
